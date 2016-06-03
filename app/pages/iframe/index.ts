e7e91eda9d5589cc8a4cb991563d7ad754d97bd7
Scott Bolinger <designbyscott@gmail.com>
2016-06-03 15:05:05 -0700

    activity modal button

--- app/pages/iframe/index.ts
+++ app/pages/iframe/index.ts
@@ -8,6 +8,7 @@ import {Page, NavParams} from 'ionic-angular';
 export default class {
     title: string;
     src: string;
+    iframe: any;
     constructor(private navParams: NavParams) {
         this.title = navParams.data.title;
         this.src = navParams.data.src;
@@ -15,5 +16,9 @@ export default class {
     }
     iframeLoaded(e) {
         console.log('iframeLoaded', e)
+    }
+    activityModal() {
+        this.iframe = document.getElementById('ap3-iframe').contentWindow;
+        this.iframe.postMessage('activity', '*');
     }
 }
