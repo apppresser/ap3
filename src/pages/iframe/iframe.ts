906882185208a2b6d6506bd4060853b9c8123063
Scott Bolinger <designbyscott@gmail.com>
2017-05-12 10:57:26 -0700

    nav.push for cart link

--- src/pages/iframe/iframe.ts
+++ src/pages/iframe/iframe.ts
@@ -368,7 +368,7 @@ export class Iframe {
     }
 
     cartLink() {
-        this.nav.setRoot(Iframe, { 'title': '', 'url': this.cart_link } );
+        this.nav.push(Iframe, { 'title': '', 'url': this.cart_link } );
     }
 
     doLogo() {
