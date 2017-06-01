420943fed6edcc5f6a0d3eedc9cf0e30d243d952
Scott Bolinger <designbyscott@gmail.com>
2017-06-01 09:54:53 -0700

    fix load more post order

--- src/pages/post-list/post-list.ts
+++ src/pages/post-list/post-list.ts
@@ -212,7 +212,7 @@ export class PostList implements OnInit {
         return;
       }
 
-      for (var i = length - 1; i >= 0; i--) {
+      for (var i = 0; i < length; ++i) {
         this.items.push( items[i] );
       }
 
