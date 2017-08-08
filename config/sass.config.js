482be6ccb215ab169266cb7fb7eff6f12eddc808
Matt Thiessen <matt@thiessen.us>
2017-08-08 13:54:38 -0500

    Revert "font-awesome"
    This reverts commit 8f6bf37c333878aa5b21712d890ddf5f3a1274a0.

--- config/sass.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  includePaths: [
-    'node_modules/ionic-angular/themes',
-    'node_modules/ionicons/dist/scss',
-    'node_modules/ionic-angular/fonts',
-    'node_modules/font-awesome/scss'
-  ]
-};
