12b1094f1c359d7728871fb8d5ed7137e89272ae
Scott Bolinger <designbyscott@gmail.com>
2017-11-13 08:16:20 -0800

    curly brackets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1014,9 +1014,9 @@ export class MyApp {
         url = redirect.url;
       }
 
-      if(!url)
+      if(!url) {
         return;
-      else if(url.indexOf('http') === -1) {
+      } else if(url.indexOf('http') === -1) {
 
         // load by page slug
 
