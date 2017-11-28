a2cbbee796cc2d6b16f0c10d751856d946dff8d3
Matt Thiessen <matt@thiessen.us>
2017-11-28 15:40:11 -0600

    'system' class links in the tab menu

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -6,12 +6,56 @@ import {NavParams, IonicPage} from 'ionic-angular';
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-  tabs: any;
+  tabs: Array<any> = [];
   mySelectedIndex: number;
 
   constructor(navParams: NavParams) {
     this.mySelectedIndex = navParams.data.tabIndex || 0;
-    this.tabs = navParams.data;
+
+    // root=null if opening in the IAB
+    for(let tab of navParams.data) {
+      let target = this.maybeOpenIAB(tab);
+      if(target) {
+        tab.root = null;
+        tab.target = target;
+      }
+      this.tabs.push(tab);
+    }
+  }
+
+  onIonSelect($event, tab) {
+    if(tab.url && tab.target) {
+      this.openIab(tab.url, tab.target);
+    }
+  }
+
+  maybeOpenIAB(tab) {
+
+    if (
+      tab.extra_classes &&
+      (tab.extra_classes.indexOf('system') >= 0 || tab.extra_classes.indexOf('external') >= 0) &&
+      tab.url &&
+      tab.url.indexOf('http') === 0
+    ) {
+      return this.getIabTarget(tab.extra_classes);
+    }
+
+    return false;
+  }
+
+  getIabTarget(extra_classes) {
+    if(extra_classes.indexOf('system') >= 0) {
+      return '_system';
+    } else if(extra_classes.indexOf('external') >= 0) {
+      return '_blank';
+    }
+    return false;
+  }
+
+  openIab( link, target, options = null ) {
+    
+    window.open(link, target, options );
+
   }
 
 }
