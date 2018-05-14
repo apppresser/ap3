98b98ed064aa5fe0ce862d11364c257e42b01fe6
Matt Thiessen <matt@thiessen.us>
2018-05-14 09:40:07 -0500

    find menu by slug or URL

--- src/pages/custom-pages/custom-page.ts
+++ src/pages/custom-pages/custom-page.ts
@@ -306,7 +306,7 @@ export class CustomPage implements OnInit, OnDestroy {
 
 	/**
 	 * Side or tab menus
-	 * @param slug page slug
+	 * @param slug page slug or URL
 	 * @param pages menu or tab pages
 	 */
 	getIndexBySlug(slug: string, pages) {
@@ -318,6 +318,8 @@ export class CustomPage implements OnInit, OnDestroy {
 
 		for(let page of pages) {
 			if(page.slug && page.slug == slug) {
+				menu_index = count;
+			} else if(page.url && page.url == slug) {
 				menu_index = count;
 			}
 			count++;
