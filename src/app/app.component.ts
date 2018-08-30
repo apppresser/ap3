8c180c7df920aca2ff2676db5ff07d6c55761bfa
Scott Bolinger <designbyscott@gmail.com>
2018-08-30 09:49:10 -0700

    buddypress colors with color picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -800,7 +800,8 @@ export class MyApp {
     styles += "p, .item p { color: "  + data.meta.design.text_color + " }";
 
     // buttons
-    styles += ".button-primary, .menu-login-button, page-login-modal div > .button, bp-modal .button { background: " + data.meta.design.button_background + "!important; color: "  + data.meta.design.button_text_color + " }";
+    styles += ".button-primary, .menu-login-button, page-login-modal div > .button, bp-modal .button, bp-list .badge, bp-details .badge, bp-messages .button { background: " + data.meta.design.button_background + "!important; color: "  + data.meta.design.button_text_color + " }";
+    styles += "bp-list .button, bp-details .button, bp-group .button, bp-profile .button { color: "  + data.meta.design.button_background + " }";
 
     // headings
     styles += "ion-page h1, ion-page h2, ion-page h3, ion-page h4, ion-page h5, ion-page h6, ion-page ion-list .item h2, ion-page ion-list .item h3, ion-page ion-list .item h4 { color: "  + data.meta.design.headings_color + " }";
