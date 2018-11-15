6cbdc65e38bafc6de5c739d6643ee603aec81f10
Scott Bolinger <designbyscott@gmail.com>
2018-11-15 11:10:50 -0800

    tabbed account page

--- src/components/woo-account/woo-account.ts
+++ src/components/woo-account/woo-account.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, ToastController } from 'ionic-angular';
 import { WooProvider } from '../../providers/woo/woo';
 import {Storage} from '@ionic/storage';
+import {InAppBrowser, InAppBrowserObject} from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'woo-account',
@@ -11,20 +12,31 @@ export class WooAccountComponent implements OnInit {
 
 	order_id: any;
 	orders: any;
+	account: any;
 	orderConfirmation: any;
 	title: string;
 	login_data: any;
 	customer: any;
+	loading: any;
+	wpUrl: string;
+	showOrdersBool: boolean = false;
+	showAccountBool: boolean = false;
 
 	constructor(
 		public navCtrl: NavController, 
 		public navParams: NavParams,
 		public wooProvider: WooProvider,
-		public storage: Storage
+		public storage: Storage,
+		public loadingCtrl: LoadingController,
+		public iab: InAppBrowser,
+		public toastCtrl: ToastController
 		) {
 	}
 
 	ngOnInit() {
+
+		let item = window.localStorage.getItem( 'myappp' );
+    	this.wpUrl = JSON.parse( item ).wordpress_url;
 
 		this.order_id = this.navParams.get('order_id');
 
@@ -43,6 +55,7 @@ export class WooAccountComponent implements OnInit {
 	getCustomerOrders() {
 
 		this.storage.get('user_login').then( login_data => {
+			console.log(login_data)
 			this.login_data = login_data
 			if( login_data && login_data.user_id ) {
 				this.customer = login_data.user_id
@@ -53,24 +66,114 @@ export class WooAccountComponent implements OnInit {
 
 	getOrder( id ) {
 
+		this.showSpinner()
+
 		this.wooProvider.get( 'orders/' + id, 'nopaging' ).then( response => {
 			console.log(response)
 			this.orderConfirmation = response
+		}).catch( e => {
+			console.warn(e)
+		}).then( ()=> {
+			this.hideSpinner()
 		})
 
 	}
 
 	getOrders( param ) {
 
+		this.showOrdersBool = true
+
+		this.showSpinner()
+
 		this.wooProvider.get( 'orders/' + param, 'nopaging' ).then( response => {
 			console.log(response)
 			this.orders = response
+		}).catch( e => {
+			console.warn(e)
+		}).then( ()=> {
+			this.hideSpinner()
 		})
 
+	}
+
+	showOrders() {
+
+		if( !this.login_data ) {
+			this.presentToast('Please login.')
+			return;
+		}
+
+		this.showAccountBool = false;
+
+		this.getOrders( '?customer=' + this.login_data.user_id + '&status=pending,processing,on-hold,completed,refunded,failed' )
+
+	}
+
+	showAccount() {
+
+		if( !this.login_data ) {
+			this.presentToast('Please login.')
+			return;
+		}
+
+		this.showSpinner()
+
+		this.showOrdersBool = false;
+		this.showAccountBool = true;
+
+		if( this.account ) {
+			this.hideSpinner()
+			return;
+		}
+
+		this.wooProvider.get( 'customers/' + this.login_data.user_id, 'nopaging' ).then( response => {
+			console.log(response)
+			this.account = response
+		}).catch( e => {
+			console.warn(e)
+		}).then( ()=> {
+			this.hideSpinner()
+		})
 	}
 
 	thanksContinue() {
 		this.navCtrl.pop()
 	}
+
+	siteLink( pageSlug ) {
+
+		this.iab.create( this.wpUrl + pageSlug, '_blank' )
+
+	}
+
+	presentToast(msg) {
+
+		let toast = this.toastCtrl.create({
+		  message: msg,
+		  duration: 3000,
+		  position: 'bottom',
+		  cssClass: 'normal-toast'
+		});
+
+		toast.present();
+
+	}
+
+	showSpinner() {
+
+        this.loading = this.loadingCtrl.create({
+            showBackdrop: false,
+            dismissOnPageChange: false
+        });
+
+        this.loading.present();
+
+    }
+
+    hideSpinner() {
+
+    	if( this.loading )
+    		this.loading.dismiss();
+    }
 
 }
